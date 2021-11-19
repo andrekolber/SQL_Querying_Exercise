ef8bf0c5e418cf8ecea01fcbb7eff5d2d6e8573a
Andre Kolber <andrekolber1@gmail.com>
2021-11-19 12:26:58 -0500

    initial commit

---
+++ queries_products.sql
@@ -0,0 +1,16 @@
+-- Comments in SQL Start with dash-dash --
+-- 1. INSERT INTO products (id, name, price, can_be_returned) VALUES('1', 'chair', 44.00, false);
+-- 2. INSERT INTO products (id, name, price, can_be_returned) VALUES('2', 'stool', 25.99, true);
+-- 3. INSERT INTO products (id, name, price, can_be_returned) VALUES('3', 'table', 124.00, false) 
+-- 4. SELECT * FROM products; 
+-- 5. SELECT name FROM products;
+-- 6. SELECT name, price FROM products;
+-- 7. INSERT INTO products (id, name, price, can_be_returned) VALUES('4', 'couch', 599.99, true);
+-- 8. SELECT * FROM products WHERE can_be_returned = 'true'
+-- 9. SELECT * FROM products WHERE price < 44.00;
+--10. SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+--11. UPDATE products SET price = (price - 20);
+--12. DELETE FROM products WHERE price < 25.00;
+--13. UPDATE products SET price = (price + 20);
+--14. UPDATE products SET can_be_returned = true;
+
